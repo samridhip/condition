59e65320b5d3184a77fba25fb1b540630996effb
samridhip <pandeysamridhi1@gmail.com>
2023-12-08 06:58:46 +0530

    condition practice

---
+++ src/Question11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Question11 {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter nth term");
+        int no=sc.nextInt();
+        int i;
+        for(i=1;i<=no;i++){
+            System.out.println(i);
+        }
+    }
+}
