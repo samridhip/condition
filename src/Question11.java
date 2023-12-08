5dcb0ac163092054cdefaeb4dee18902d86ad19e
samridhip <pandeysamridhi1@gmail.com>
2023-12-08 07:01:27 +0530

    condition practice

--- src/Question11.java
+++ src/Question11.java
@@ -6,8 +6,11 @@ public class Question11 {
         System.out.println("Enter nth term");
         int no=sc.nextInt();
         int i;
+        int sum=0;
         for(i=1;i<=no;i++){
             System.out.println(i);
+            sum+=i;
         }
+        System.out.println("The sum of n natural number is "+sum);
     }
 }
