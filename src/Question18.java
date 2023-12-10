8f1cf031cae37beb8e6e8afeb9e2226b0f5bf0d0
samridhip <pandeysamridhi1@gmail.com>
2023-12-10 22:59:47 +0530

    condition practice

---
+++ src/Question18.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Question18 {
+    public static void main(String[]args){
+        int i,n,j,k=1;
+        System.out.println("Enter the nos of n");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for(i=1;i<=n;i++){
+            for(j=1;j<=i;j++){
+                System.out.print(k++);
+                System.out.println("");
+            }
+        }
+    }
+}
